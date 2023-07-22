0d0a8d78685088fff0b9ca6a552489352574bc27
daniele <danielegarofalo1993@gmail.com>
2023-07-22 22:55:56 +0200

    beauty html

--- myTopnav.js
+++ myTopnav.js
@@ -1,13 +1,12 @@
 var myTopnav="";
 
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
-myTopnav = myTopnav + "";
+myTopnav = myTopnav + "<div class='topnav' id='myTopnav' >";
+myTopnav = myTopnav + "<a href='INDEX.HTML' class='active'>Home</a>";
+myTopnav = myTopnav + "<a href='news.html'>News</a>";
+myTopnav = myTopnav + "<a href='contatti.html'>Contact</a>";
+myTopnav = myTopnav + "<a href='Vantaggi.html'>Vantaggi</a>";
+myTopnav = myTopnav + "<a href='contatti.html'>Distribuzione</a>";
+myTopnav = myTopnav + "<a href='contatti.html'>Promozioni</a>";
+myTopnav = myTopnav + "<a href='ProvaPagamento/Login.html' class='taco'>";
+myTopnav = myTopnav + "<a href='ProvaPagamento/Login.html' class='taco'>login</a>";
+myTopnav = myTopnav + "</div>";
