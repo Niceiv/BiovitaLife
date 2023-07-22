caa1a9d97fc309510be4e9705e25c59e97422616
Niceiv <139993964+Niceiv@users.noreply.github.com>
2023-07-22 23:04:18 +0200

    Add files via upload
    Modifica totale a tutti i file
    JS e CSS spostati in sotto cartelle

---
+++ JS/myTopnav.js
@@ -0,0 +1,17 @@
+var myTopnav="";
+
+
+myTopnav = myTopnav + "<DIV class='top' >";
+myTopnav = myTopnav + "    <a href='INDEX.HTML'> <img src='image/LogoSfondi/Logo.PNG'></a>";
+myTopnav = myTopnav + "</DIV>";
+myTopnav = myTopnav + "<div class='topnav' id='myTopnav' >";
+myTopnav = myTopnav + "    <a href='INDEX.HTML' class='active'>Home</a>";
+myTopnav = myTopnav + "    <a href='news.html'>News</a>";
+myTopnav = myTopnav + "    <a href='contatti.html'>Contact</a>";
+myTopnav = myTopnav + "    <a href='Vantaggi.html'>Vantaggi</a>";
+myTopnav = myTopnav + "    <a href='contatti.html'>Distribuzione</a>";
+myTopnav = myTopnav + "    <a href='contatti.html'>Promozioni</a>";
+myTopnav = myTopnav + "    <a href='ProvaPagamento/Login.html' class='taco'>";
+myTopnav = myTopnav + "    <i class='bi bi-person'>Login</i>";
+myTopnav = myTopnav + "    </a>";
+myTopnav = myTopnav + "</div>";
