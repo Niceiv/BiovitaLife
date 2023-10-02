3b1e2d9d610894c70ba484c52fb5018763a11e80
MMascherin <massimiliano.mascherin@gmail.com>
2023-10-02 15:39:53 +0200

    Passaggio parametri via AJAX
    Provare la pagina modal2.html per verificare il funzionamento 
    Va rivista nell'ottica degli Olii

---
+++ dati.php
@@ -0,0 +1,22 @@
+<?php
+
+    
+    $idDati=$_POST["idDati"];
+    
+     
+    echo "<br>idDati:$idDati";
+
+    switch ($idDati) {
+        case '1':
+            echo '<br>Valore inserito 1';
+            break;
+        case '2':
+            echo '<br>Valore inserito 2';
+            break;        
+        case '3':
+            echo '<br>Valore inserito 3';
+            break;
+        default:
+            echo '<br>Manca il valore';
+            break;
+    }
