940ebe71c1a4806372ba009aca382a1c73891be2
Daniele <danielegarofalo1993@gmail.com>
2023-10-03 14:49:30 +0200

    box olii

--- Olii.php
+++ Olii.php
@@ -9,6 +9,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+        type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/boxservice.css">
 
     <link rel="icon" type="image/vnd.icon" href="image/LogoSfondi/Logo.ico">
 
@@ -19,12 +25,6 @@
     <script src="JS/index.js"></script>
     <script src="JS/myTopnav.js"></script>
     <title>Document</title>
-</head>
-
-<body>
-    <script language="javaScript">
-        document.write(myTopnav);
-    </script>
 
     <style>
         /* The Modal (background) */
@@ -74,12 +74,212 @@
             text-decoration: none;
             cursor: pointer;
         }
+
+        body {
+            background-image: url(image/LogoSfondi/sfondoOlii.png);
+            color: black;
+            background-repeat: no-repeat;
+            background-position: top center;
+            background-attachment: scroll;
+        }
     </style>
+</head>
+
+<body>
+    <script language="javaScript">
+        document.write(myTopnav);
+    </script>
+
+
     <div class="container " style="margin-top: 120px;">
 
-        <h1 class="text-center">Olii Essenziali</h1>
-        <div class="row" style="position:absolute; top:450px; right:30px">
-            <div class="col-4"><img src="image/foto/olio.png" alt="Olio" style="border-radius:50%"></div>
+        <h1 class="text-center">OliMonde</h1>
+
+
+
+
+
+        <div class="row">
+            <div class="col-md-3 col-sm-6 ">
+                <div class="service-box">
+                    <div class="service-icon yellow">
+                        <div class="front-content">
+                            <h3>design</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>design</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 ">
+                <div class="service-box">
+                    <div class="service-icon orange">
+                        <div class="front-content">
+                            <h3>php</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>php developer</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="service-box ">
+                    <div class="service-icon red">
+                        <div class="front-content">
+                            <h3>Ui Developer</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>Developer</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="service-box">
+                    <div class="service-icon grey">
+                        <div class="front-content">
+
+                            <h3>java script</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>java script</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 col-sm-6 ">
+                <div class="service-box">
+                    <div class="service-icon yellow">
+                        <div class="front-content">
+                            <h3>design</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>design</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 ">
+                <div class="service-box">
+                    <div class="service-icon orange">
+                        <div class="front-content">
+
+                            <h3>php</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>php developer</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="service-box ">
+                    <div class="service-icon red">
+                        <div class="front-content">
+                            <h3>Ui Developer</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>Developer</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="service-box">
+                    <div class="service-icon grey">
+                        <div class="front-content">
+
+                            <h3>java script</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>java script</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 col-sm-6 ">
+                <div class="service-box">
+                    <div class="service-icon yellow">
+                        <div class="front-content">
+                            <h3>design</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>design</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 ">
+                <div class="service-box">
+                    <div class="service-icon orange">
+                        <div class="front-content">
+
+                            <h3>php</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>php developer</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="service-box ">
+                    <div class="service-icon red">
+                        <div class="front-content">
+                            <h3>Ui Developer</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>Developer</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="service-box">
+                    <div class="service-icon grey">
+                        <div class="front-content">
+
+                            <h3>java script</h3>
+                        </div>
+                    </div>
+                    <div class="service-content">
+                        <h3>java script</h3>
+                        <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because
+                            those who do not know how to pursue pleasure</p>
+                    </div>
+                </div>
+            </div>
         </div>
         <?php
 
@@ -95,8 +295,7 @@
             while ($row_nome = $res_nome->fetch_assoc()) {
 
                 $idOlii = $row_nome['idolii'];
-                echo "<div class='row'>";
-                echo "<div class='col-8'>";
+
 
                 echo '<h4>' . $row_nome['nome'] . '</h4>';
 
@@ -113,8 +312,7 @@
                 echo '<br><button  onclick="mostra(' . $idOlii . ')"/>mostra di più...</button>';
 
                 echo '<hr>';
-                echo "</div>";
-                echo "</div>";
+
 
 
             }
