93db0c58c0f592b4c5d86b464b87c0740ad4d304
MMascherin <massimiliano.mascherin@gmail.com>
2023-11-03 10:17:28 +0100

    JS da sito

--- JS/MyFooter.js
+++ JS/MyFooter.js
@@ -1,42 +1,42 @@
-var myFooter = "";
-
-
-myFooter = myFooter + "<footer class='footer-distributed'>";
-myFooter = myFooter + "	<div class='footer-left'>";
-myFooter = myFooter + "		<h3>Biovita<span>Life</span></h3>";
-myFooter = myFooter + "		<p class='footer-links'>";
-myFooter = myFooter + "			<a href='INDEX.HTML' class='link-1'>Home</a>";
-myFooter = myFooter + "			<a href='Privacy.html'>Privacy</a>";
-myFooter = myFooter + "			<a href='lineeGuida.html'>Linee Guida</a>";
-myFooter = myFooter + "		</p>";
-myFooter = myFooter + "		<p class='footer-company-name'>Biovita Life © 2023</p>";
-myFooter = myFooter + "	</div>";
-myFooter = myFooter + "	<div class='footer-center'>";
-myFooter = myFooter + "		<div>";
-myFooter = myFooter + "			<i class='fa fa-map-marker'></i>";
-myFooter = myFooter + "			<p><span>Via Caio C. Nepote 50</span>Marco Simone, Guidonia Montecelio (RM)</p>";
-myFooter = myFooter + "		</div>";
-myFooter = myFooter + "		<div>";
-myFooter = myFooter + "			<a href='tel:+390774367075'><i class='fa fa-phone'></i></a>";
-myFooter = myFooter + "			<p> <a href='tel:+390774367075'>+39 0774367075</a></p>";
-myFooter = myFooter + "		</div>";
-myFooter = myFooter + "		<div>";
-myFooter = myFooter + "			<a href='mailto:info@biovitalife.it'><i class='fa fa-envelope'></i></a>";
-myFooter = myFooter + "			<p><a href='mailto:info@biovitalife.it'>info@biovitalife.it</a></p>";
-myFooter = myFooter + "		</div>";
-myFooter = myFooter + "	</div>";
-myFooter = myFooter + "	<div class='footer-right'>";
-myFooter = myFooter + "		<p class='footer-company-about'>";
-myFooter = myFooter + "			<span>La nostra azienda</span>";
-myFooter = myFooter + "			Siamo un'azienda familiare specializzata in prodotti naturali.";
-myFooter = myFooter + "		</p>";
-/*
-myFooter = myFooter + "		<div class='footer-icons'>";
-myFooter = myFooter + "			<a href='#'><i class='fa fa-facebook'></i></a>";
-myFooter = myFooter + "			<a href='#'><i class='fa fa-twitter'></i></a>";
-myFooter = myFooter + "			<a href='#'><i class='fa fa-linkedin'></i></a>";
-myFooter = myFooter + "			<a href='#'><i class='fa fa-github'></i></a>";
-myFooter = myFooter + "		</div>";
-*/
-myFooter = myFooter + "	</div>";
-myFooter = myFooter + "</footer>";
+var myFooter = "";
+
+
+myFooter = myFooter + "<footer class='footer-distributed'>";
+myFooter = myFooter + "	<div class='footer-left'>";
+myFooter = myFooter + "		<h3>Biovita<span>Life</span></h3>";
+myFooter = myFooter + "		<p class='footer-links'>";
+myFooter = myFooter + "			<a href='INDEX.HTML' class='link-1'>Home</a>";
+myFooter = myFooter + "			<a href='Privacy.html'>Privacy</a>";
+myFooter = myFooter + "			<a href='lineeGuida.html'>Linee Guida</a>";
+myFooter = myFooter + "		</p>";
+myFooter = myFooter + "		<p class='footer-company-name'>Biovita Life © 2023</p>";
+myFooter = myFooter + "	</div>";
+myFooter = myFooter + "	<div class='footer-center'>";
+myFooter = myFooter + "		<div>";
+myFooter = myFooter + "			<i class='fa fa-map-marker'></i>";
+myFooter = myFooter + "			<p><span>Via Caio C. Nepote 50</span>Marco Simone, Guidonia Montecelio (RM)</p>";
+myFooter = myFooter + "		</div>";
+myFooter = myFooter + "		<div>";
+myFooter = myFooter + "			<a href='tel:+390774367075'><i class='fa fa-phone'></i></a>";
+myFooter = myFooter + "			<p> <a href='tel:+390774367075'>+39 0774367075</a></p>";
+myFooter = myFooter + "		</div>";
+myFooter = myFooter + "		<div>";
+myFooter = myFooter + "			<a href='mailto:info@biovitalife.it'><i class='fa fa-envelope'></i></a>";
+myFooter = myFooter + "			<p><a href='mailto:info@biovitalife.it'>info@biovitalife.it</a></p>";
+myFooter = myFooter + "		</div>";
+myFooter = myFooter + "	</div>";
+myFooter = myFooter + "	<div class='footer-right'>";
+myFooter = myFooter + "		<p class='footer-company-about'>";
+myFooter = myFooter + "			<span>La nostra azienda</span>";
+myFooter = myFooter + "			Siamo un'azienda familiare specializzata in prodotti naturali.";
+myFooter = myFooter + "		</p>";
+/*
+myFooter = myFooter + "		<div class='footer-icons'>";
+myFooter = myFooter + "			<a href='#'><i class='fa fa-facebook'></i></a>";
+myFooter = myFooter + "			<a href='#'><i class='fa fa-twitter'></i></a>";
+myFooter = myFooter + "			<a href='#'><i class='fa fa-linkedin'></i></a>";
+myFooter = myFooter + "			<a href='#'><i class='fa fa-github'></i></a>";
+myFooter = myFooter + "		</div>";
+*/
+myFooter = myFooter + "	</div>";
+myFooter = myFooter + "</footer>";
