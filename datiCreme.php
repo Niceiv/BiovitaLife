d761af59dbdabdf6a3b26e839f3e964e5760ff86
Daniele <danielegarofalo1993@gmail.com>
2023-11-15 21:32:28 +0100

    eliminazione linee

--- datiCreme.php
+++ datiCreme.php
@@ -1,138 +1,69 @@
 <?php
 header('Content-Type: text/html; charset=ISO-8859-1');
- 
 
 
 $idDati = $_POST["idDati"];
 
-
-
 error_reporting(E_ERROR | E_PARSE);
 
-
-
-        require(__DIR__.'\Config\SQL_command.php');
-
-
-
+require(__DIR__ . '\Config\SQL_command.php');
 
 
 $sql_nome = "SELECT * FROM `vw_prodotti` where idProdotto=$idDati";
-
 $res_nome = GetData($sql_nome);
-
 if ($res_nome->num_rows > 0) {
-
     while ($row_nome = $res_nome->fetch_assoc()) {
-
         $idProdotto = $row_nome['idProdotto'];
 
-
-
         echo "<div class='row'";
-
         echo "  <div class='col-sm-12'>";
-
         echo '      <H4>' . $row_nome['DescProd'] . '</H4>';
-
         echo "  </div>";
-
         echo "</div>";
 
-        
-
         echo "<div class='row'";
-
         echo "  <div class='col-sm-8'>";
-
         echo '      <h5><b>Descrizione</b></h5>';
-
         echo '      <p>' . $row_nome['Descrizione'] . '</p>';
-  $prz = sprintf('%01.2f', $row_nome["Prezzo"]) . ' ' . chr(128);
-
-        echo '      <h6>' . $prz . '</h6>'; 
-
+        $prz = sprintf('%01.2f', $row_nome["Prezzo"]) . ' ' . chr(128);
+        echo '      <h6>' . $prz . '</h6>';
         echo "  </div>";
-
         echo "  <div class='col-sm-4'>";
-
         echo "      <img src='" . $row_nome['img'] . "'alt='" . $row_nome['DescProd'] . "' style='width:250px;height:250px;'>";
-
         echo "  </div>";
-
         echo "</div>";
 
 
-
-
-
         echo "<div class='row'";
-
         echo "  <div class='col-sm-12'>";
-
-
 
         if ($row_nome['ingredienti'] != '') {
-
             echo '<h5><b>Ingredienti</b></h5>';
-
             echo "<p>" . $row_nome['ingredienti'] . "</p>";
-
         } else {
-
             //BENEFICI
-
             $sql_prod_dati = "SELECT * FROM `prodotti_dati` WHERE idProdotto = $idProdotto";
-
             $res_prod_dati = GetData($sql_prod_dati);
-
             if ($res_prod_dati->num_rows > 0) {
-
                 echo '<h5><b>Ingredienti</b></h5>';
-
                 echo '<ul>';
-
                 while ($row_prod_dati = $res_prod_dati->fetch_assoc()) {
-
                     echo '<LI>';
-
                     echo '<strong>' . $row_prod_dati['desc'] . ': </strong>';
-
                     echo $row_prod_dati['prop'];
-
                     echo '</LI>';
 
-
-
                 }
-
                 echo '</ul>';
-
             }
-
-
 
         }
 
-
-
         echo '<h5><b>Modalit&agrave; di utilizzo</b></h5>';
-
         echo '<p>' . $row_nome['utilizzo'] . '</p>';
 
-
-
         echo "  </div>";
-
         echo "</div>";
-
-
-
-
-
-
-
-
 
 
 
@@ -142,12 +73,7 @@ if ($res_nome->num_rows > 0) {
 
     }
 
-
-
 }
-
-
-
 
 
 ?>
--- datiOlii.php
+++ datiOlii.php
@@ -1,198 +1,101 @@
-
-
 <?php
 header('Content-Type: text/html; charset=ISO-8859-1');
- 
 
 
 $idDati = $_POST["idDati"];
 
-
-
 error_reporting(E_ERROR | E_PARSE);
-
-        require(__DIR__.'\Config\SQL_command.php');
- 
-
+require(__DIR__ . '\Config\SQL_command.php');
 
 
 
 $sql_nome = "SELECT * FROM `olii` where idolii=$idDati";
-
 $res_nome = GetData($sql_nome);
-
 if ($res_nome->num_rows > 0) {
-
     while ($row_nome = $res_nome->fetch_assoc()) {
 
 
-
-
-
         $idOlii = $row_nome['idolii'];
-
-        echo '<H4>'. $row_nome['nome'] . '</H4>';
-
-
+        echo '<H4>' . $row_nome['nome'] . '</H4>';
 
         $Img = $row_nome['IMG'];
 
-
-
         /*
-
         echo '<table border=1><tr>';
-
         if ($Img != '') {
-
             echo "<td  width='10%' >";
-
             $srcImg = "image/foto/$Img";
-
             echo "<image  width='100%' src='$srcImg' border=1 />";
-
             echo '</td>';
-
         }
-
         
-
         if ($Img == '') {
-
             echo "<td width='100%' valign='top'>";
-
         } else {
-
             echo "<td width='90%'  valign='top'>";
-
         }
-
         */
-
         //DESCRIZIONE
-
         $sql_olii_desc = "SELECT * FROM `olii_desc` WHERE idOlii = $idOlii and idInfoOli=1";
-
         $res_olii_desc = GetData($sql_olii_desc);
-
         if ($res_olii_desc->num_rows > 0) {
-
             $row_olii_desc = $res_olii_desc->fetch_assoc();
-
             echo '<h5><b>Descrizione</b></h5>';
-
             echo $row_olii_desc['Descrizione'];
 
-
-
         }
-
         //echo '</td>';
-
-
-
 
 
         //echo '</tr></table>';
 
-
-
         //BENEFICI
-
         $sql_olii_benefici = "SELECT * FROM `olii_ben_prop` WHERE idOlii = $idOlii and idInfoOlii=2";
-
         $res_olii_benefici = GetData($sql_olii_benefici);
-
         if ($res_olii_benefici->num_rows > 0) {
-
             echo '<h5><b>Benefici</b></h5>';
-
             echo '<ul>';
-
             while ($row_olii_benefici = $res_olii_benefici->fetch_assoc()) {
-
                 echo '<LI>';
-
                 echo '<b>' . $row_olii_benefici['Ben_Prop'] . '</b>';
-
                 echo $row_olii_benefici['Descrizione'];
-
                 echo '</LI>';
 
-
-
             }
-
             echo '</ul>';
-
         }
-
-
 
         //PROPRIETA
-
         $sql_olii_prop = "SELECT * FROM `olii_ben_prop` WHERE idOlii = $idOlii and idInfoOlii=3";
-
         $res_olii_prop = GetData($sql_olii_prop);
-
         if ($res_olii_prop->num_rows > 0) {
-
             echo '<h5><b>Propriet&agrave;</b></h5>';
-
             echo '<ul>';
-
             while ($row_olii_prop = $res_olii_prop->fetch_assoc()) {
-
                 echo '<LI>';
-
                 echo '<b>' . $row_olii_prop['Ben_Prop'] . '</b>';
-
                 echo $row_olii_prop['Descrizione'];
-
                 echo '</LI>';
 
-
-
             }
-
             echo '</ul>';
-
         }
-
-
-
 
 
         //AVVERTENZE
-
         $sql_olii_avv = "SELECT * FROM `olii_desc` WHERE idOlii = $idOlii and idInfoOli=4";
 
-
-
         $res_olii_avv = GetData($sql_olii_avv);
-
         if ($res_olii_avv->num_rows > 0) {
-
             $row_olii_avv = $res_olii_avv->fetch_assoc();
-
             echo '<h5><b>Avvertenze</b></h5>';
-
             echo $row_olii_avv['Descrizione'];
-
-
 
         }
 
-
-
     }
 
-
-
 }
-
-
-
 
 
 ?>
