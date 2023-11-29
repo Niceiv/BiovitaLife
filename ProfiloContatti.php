0367d26fecbdd1700681a6902173a97264f96903
Daniele <danielegarofalo1993@gmail.com>
2023-11-29 19:41:12 +0100

    insert contatti e indirizzi

--- ProfiloContatti.php
+++ ProfiloContatti.php
@@ -1,4 +1,41 @@
-<?php ?>
+<?php
+
+
+if ($actUpd == 'AggiungiRecapito') {
+
+
+    $Rec_Recapito = $_POST['rec_recapito'];
+    $Rec_TipoRecap = $_POST['rec_tipo_recapito'];
+    $Rec_PredRecap = $_POST['rec_checkbox_pred'];
+    $Rec_denom_recapito = isset($_POST['rec_denom_recapito']);
+
+
+
+
+    echo "<BR>Recapito: $Rec_Recapito";
+    echo "<BR>Tipo Recapito: $Rec_TipoRecap";
+    echo "<BR>IdIndirizzo: $Rec__IdIndirizzo";
+    echo "<BR>Default: $Rec_PredRecap";
+
+
+
+    $sql_ins_recap = "INSERT INTO `recapiti` 
+    (`idutente`,
+    `recapito`,
+    `idtipo_recapito`,
+    `idindirizzo`,
+    `default`
+    ) VALUES
+    ($idutente,
+    '$Rec_Recapito',
+    $Rec_TipoRecap,
+    $Rec_denom_recapito,
+    $Rec_PredRecap); ";
+
+    ExecuteSQL($sql_ins_recap);
+}
+
+?>
 <!--pulsante aggiungi contatto -->
 <h2 class="text-center">Informazioni di contatto</h2>
 <div class="row">
@@ -30,8 +67,8 @@
                 <div class="grid-container">
                     <div class="grid-item">
                         <div class="form-group">
-                            <label for="tipo_recapito" class="mb-3">Tipo di contatto:</label>
-                            <select id="tipo_recapito" name="tipo_recapito" class="mb-3" required>
+                            <label for="rec_tipo_recapito" class="mb-3">Tipo di contatto:</label>
+                            <select id="rec_tipo_recapito" name="rec_tipo_recapito" class="mb-3" required>
                                 <option value="0" class="text-center">-</option>
                                 <?php
                                 $sql_tipo_rec = "SELECT * FROM tipo_recapito";
@@ -52,8 +89,8 @@
                     <div class="grid-item"><b>PER</b></div>
                     <div class="grid-item">
                         <div class="form-group">
-                            <label for="denom_recapito" class="mb-3">Denominazione:</label>
-                            <select id="denom_recapito" name="denom_recapito" class="mb-3" required>
+                            <label for="rec_denom_recapito" class="mb-3">Denominazione:</label>
+                            <select id="rec_denom_recapito" name="rec_denom_recapito" class="mb-3" required>
                                 <option value="0">Generale</option>
                                 <?php
                                 $sql_denom_rec = "SELECT  idindirizzo, denominazione FROM indirizzi WHERE idutente =  $idutente";
@@ -73,15 +110,23 @@
                     </div>
                     <div class="grid-item-full">
                         <div class="form-group">
-                            <label for="recapito">Recapito:</label>
-                            <input type="text" class="form-control" id="recapito" name="recapito"
+                            <label for="rec_recapito">Recapito:</label>
+                            <input type="text" class="form-control" id="rec_recapito" name="rec_recapito"
                                 placeholder="Inserisci il recapito" required>
                         </div>
                     </div>
                 </div>
+                <div class="row ml-4">
+                    <input type="hidden" id="rec_checkbox_pred" name="rec_checkbox_pred" value="0">
+                    <label for="rec_checkbox_pred" class="checkbox-inline"><input type="checkbox" id="rec_checkbox_pred"
+                            name="rec_checkbox_pred" value="1">Imposta contatto come
+                        predefinito</label>
+
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="submit" class="btn btn-default left">Aggiungi</button>
+                <button type="submit" class="btn btn-default left"
+                    onclick="AggiornaProfilo('AggiungiRecapito')">Aggiungi</button>
 
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
--- ProfiloIndirizzo.php
+++ ProfiloIndirizzo.php
@@ -1,4 +1,48 @@
-<?php ?>
+<?php
+if ($actUpd == 'AggiungiIndirizzo') {
+
+
+    $nuovaDenInd = ucwords($_POST['ind_denominazione']);
+    $nuovoTipoInd = $_POST['tipo_indirizzo'];
+    $nuovoInd = $_POST['ind_indirizzo'];
+    $nuovoCap = $_POST['ind_cap'];
+    $nuovaProv = $_POST['ind_provincia'];
+    $nuovaCitta = $_POST['ind_citta'];
+    $nuovoPredInd = $_POST['ind_checkbox_pred'];
+
+    echo "<BR>Denominazione: $nuovaDenInd";
+    echo "<BR>Tipo Indirizzo: $nuovoTipoInd";
+    echo "<BR>Indirizzo $nuovoInd";
+    echo "<BR>Cap: $nuovoCap";
+    echo "<BR>Provincia: $nuovaProv";
+    echo "<BR>Città: $nuovaCitta";
+    echo "<BR>Default: $nuovoPredInd";
+
+
+
+    $sql_ins_ind = "INSERT INTO `indirizzi` 
+    (`idutente`,
+    `denominazione`,
+    `idtipo_indirizzo`,
+    `indirizzo`,
+    `cap`,
+    `provincia`,
+    `citta`,
+    `default`
+    ) VALUES
+    ($idutente,
+    '$nuovaDenInd',
+    $nuovoTipoInd,
+    '$nuovoInd',
+    $nuovoCap,
+    '$nuovaProv',
+    '$nuovaCitta',
+    $nuovoPredInd); ";
+
+    ExecuteSQL($sql_ins_ind);
+}
+
+?>
 <!--pulsante aggiungi indirizzo -->
 <h2 class="text-center">I Tuoi Indirizzi</h2>
 <div class="row">
@@ -32,7 +76,7 @@
                     <div class="grid-item-semi-full">
                         <div class="form-group">
                             <label for="tipo_indirizzo" class="mb-3">Tipo di indirizzo:</label>
-                            <select id="tipo_indirizzo" class="mb-3" required>
+                            <select id="tipo_indirizzo" name="tipo_indirizzo" class="mb-3" required>
                                 <?php
 
                                 $sql_tipo_ind = "SELECT * FROM tipo_indirizzo WHERE IDTipoPersona=$IDTipoPersona";
@@ -45,9 +89,6 @@
                                         $TipoIndirizzo = $rowTipoInd['tipo_indirizzo'];
                                         $options = "<option value='$IdTipoIndirizzo'>" . $TipoIndirizzo . "</option>";
                                         echo $options;
-
-
-
                                     }
                                 }
                                 ?>
@@ -58,8 +99,8 @@
 
                     <div class="grid-item-full">
                         <div class="form-group">
-                            <label for="denominazione">Denominazione:</label>
-                            <input type="text" class="form-control" id="denominazione" name="denominazione"
+                            <label for="ind_denominazione">Nominativo:</label>
+                            <input type="text" class="form-control" id="ind_denominazione" name="ind_denominazione"
                                 placeholder="Inserisci il nominativo" required>
                         </div>
                     </div>
@@ -68,8 +109,8 @@
 
                     <div class="grid-item-full">
                         <div class="form-group">
-                            <label for="indirizzo">Indirizzo:</label>
-                            <input type="text" class="form-control" id="indirizzo" name="indirizzo"
+                            <label for="ind_indirizzo">Indirizzo e nr civico:</label>
+                            <input type="text" class="form-control" id="ind_indirizzo" name="ind_indirizzo"
                                 placeholder="Inserisci la via" required>
                         </div>
                     </div>
@@ -78,15 +119,15 @@
                 <div class="row">
                     <div class="col-md-3 col-sm-3 ml-4">
                         <div class="form-group">
-                            <label for="cap">CAP:</label>
-                            <input type="text" class="form-control" name="cap" id="cap" placeholder="CAP" required
-                                maxlength="5" pattern="[0-9]{5}">
+                            <label for="ind_cap">CAP:</label>
+                            <input type="text" class="form-control" name="ind_cap" id="ind_cap" placeholder="CAP"
+                                required maxlength="5" pattern="[0-9]{5}">
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-3 ml-4">
                         <div class="form-group">
-                            <label for="provincia" class="mb-3">Provincia:</label>
-                            <select id="provincia" name="provincia" class="mb-3" required>
+                            <label for="ind_provincia" class="mb-3">Provincia:</label>
+                            <select id="ind_provincia" name="ind_provincia" class="mb-3" required>
                                 <option value="RM">Roma</option>
                             </select>
                         </div>
@@ -94,8 +135,8 @@
                     </div>
                     <div class="col-md-3 col-sm-3 ml-4">
                         <div class="form-group">
-                            <label for="tipo_indirizzo" class="mb-3">Città:</label>
-                            <select id="tipo_indirizzo" class="mb-3" required>
+                            <label for="ind_citta" class="mb-3">Città:</label>
+                            <select id="ind_citta" name="ind_citta" class="mb-3" required>
                                 <option value="RM">Roma</option>
 
 
@@ -104,10 +145,17 @@
                     </div>
 
                 </div>
+                <div class="row ml-4">
+                    <input type="hidden" id="ind_checkbox_pred" name="ind_checkbox_pred" value="0">
+                    <label for="ind_checkbox_pred" class="checkbox-inline"><input type="checkbox" id="ind_checkbox_pred"
+                            name="ind_checkbox_pred" value="1">Imposta indirizzo come
+                        predefinito</label>
+                </div>
             </div>
 
             <div class="modal-footer">
-                <button type="submit" class="btn btn-default left">Aggiungi</button>
+                <button type="submit" class="btn btn-default left"
+                    onclick="AggiornaProfilo('AggiungiIndirizzo')">Aggiungi</button>
 
                 <button type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
             </div>
@@ -115,3 +163,87 @@
 
     </div>
 </div>
+<!--
+    
+<?php
+/*
+$riga = true;
+
+
+$sql_sel_ind = "SELECT * FROM indirizzi WHERE idutente=$idutente";
+$res_sel_ind = GetData($sql_sel_ind);
+
+$info_den_ind = $row["denominazione"];
+$info_den_ind = $row["denominazione"];
+$info_den_ind = $row["denominazione"];
+$info_den_ind = $row["denominazione"];
+$info_den_ind = $row["denominazione"];
+$info_den_ind = $row["denominazione"];
+
+$sql_sel_tipo_ind = "SELECT * FROM tipo_indirizzo WHERE idtipo_indirizzo=$idtipo_indirizzo";
+$res_sel_ind = GetData($sql_sel_tipo_ind);
+
+$info_tipo_ind = $row["tipo_indirizzo"];
+
+
+if ($res_sel_ind->num_rows > 0) {
+    while ($row_ind = $res_sel_ind->fetch_assoc()) {
+
+        if ($riga) {
+            echo "<div class='row'>";
+            $colonna = 1;
+            $riga = false;
+        }
+
+
+        ?>
+        <div class="col-md-4">
+            <div class="card ">
+                <?php
+                if ($row['default'] == 1) {
+
+                    echo "<div class='card-header text-center'>Indirizzo Predefinito</div>";
+
+                } else {
+                    echo "<div class='card-header text-center'>Altro Indirizzo</div>";
+                }
+
+                ?>
+
+                <div class="card-body">
+                    <div class="info-ind">
+                        <p>
+                            <?= $info_den_ind ?>
+                        </p>
+                        <p>
+                            <?= $info_tipo_ind ?>
+                        </p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <a class="link-ind">Modifica</a>
+
+                    <a class="link-ind">Imposta come predefinito</a>
+                </div>
+
+                <?php
+                $colonna++;
+
+                if ($colonna == 4) {
+                    $riga = true;
+                }
+
+
+                if ($riga) {
+                    echo "</div>";
+                }
+    }
+}
+*/
+?>
+    </div>
+</div>
+
+-->
