1ee3f245c1502466372a0dcbebe95680bf3bc972
MMascherin <massimiliano.mascherin@gmail.com>
2023-12-14 17:52:42 +0100

    Update ProfiloIndirizzo.php
    GEstione combo a cascata

--- ProfiloIndirizzo.php
+++ ProfiloIndirizzo.php
@@ -1,4 +1,117 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script>
+    $(document).ready(function() {
+
+        //Leggo eventuali valori inseriti in una sessione precedente
+        var IDReg = $('#PreSelReg').val();
+        var IDPrv = $('#PreSelPrv').val();
+        var IDCom = $('#PreSelCom').val();
+        var IDFrz = $('#PreSelFrz').val();
+
+        //console.log('start\r\n');
+
+
+
+        //Se IDPRV è valorizzata vuol dire che il dato è stato precedentemente salvato a db
+        if (IDPrv != '') {
+            //var strEcho = 'Reg: ' + IDReg + '\r\nPrv: ' + IDPrv + '\r\nCom: ' + IDCom + '\r\nFrz: ' + IDFrz;
+            //console.log(strEcho);
+            AjaxCall('ALL', IDReg, IDPrv, IDCom, IDFrz);
+        }
+
+
+        //Scelta della regione
+        $('#cbo_reg').change(function() {
+            //Regione selezionata
+            IDReg = $('#cbo_reg').val();
+            console.log('Regione selezionata: ' + IDReg);
+            AjaxCall('REG', IDReg, IDPrv, IDCom, IDFrz);
+
+        }); //Fine cbo_reg
+
+        //Selta della provincia
+        $('#cbo_prv').change(function() {
+            //Regione selezionata
+
+            IDPrv = $('#cbo_prv').val();
+            console.log('Provincia selezionata: ' + IDPrv);
+            AjaxCall('PRV', IDPrv, IDPrv, IDCom, IDFrz);
+
+        }); //Fine cbo_prv
+
+
+        //Selta della comune
+        $('#cbo_com').change(function() {
+            //Regione selezionata
+            IDCom = $('#cbo_com').val();
+            console.log('Comune selezionata: ' + IDCom);
+            AjaxCall('COM', IDCom, IDPrv, IDCom, IDFrz);
+
+        }); //Fine cbo_prv
+
+    }); //Fine document
+
+    function AjaxCall(Tipo, ValSel, PrvSel, ComSel, FrzSel) {
+        console.log('\r\nAjaxCall2');
+        var strEcho = '\r\nTipo: ' + Tipo + '\r\nValSel: ' + ValSel + '\r\nPrv: ' + PrvSel + '\r\nCom: ' + ComSel + '\r\nFrz: ' + FrzSel;
+        console.log(strEcho);
+
+
+        $.ajax({
+            type: "POST",
+            url: "datiRepPrvComFrz.php",
+            data: {
+                'TIPO': Tipo,
+                'VAL_SEL': ValSel,
+                'PRV_SEL': PrvSel,
+                'COM_SEL': ComSel,
+                'FRZ_SEL': FrzSel
+            },
+            dataType: "JSON",
+
+            success: function(data) {
+
+                if (data.PRV != null) {
+                    //console.log('\r\nPRV:' + data.PRV);
+                    $('#cbo_prv').html(data.PRV);
+                }
+
+                if (data.COM != null) {
+                    //console.log('\r\COM:' + data.COM);
+                    $('#cbo_com').html(data.COM);
+                }
+
+                if (data.FRZ != null) {
+                    //console.log('\r\FRZ:' + data.FRZ);
+                    $('#cbo_frz').html(data.FRZ);
+                }
+
+
+            },
+
+            error: function() {
+                alert("Chiamata fallita, si prega di riprovare...");
+            }
+        }); //Fine Ajax Prov
+    }
+</script>
+
 <?php
+
+
+//Fingo di avere dei valori letti da DBase
+//QUando nel db NON ci sono dati le varibili devono essere instanziate a zero
+
+
+$IDReg = 204;
+$IDProv = 26;
+$IDCom = 26010;
+$IDFrz = 103641;
+
+$IDReg = 0;
+$IDProv = 0;
+$IDCom = 0;
+$IDFrz = 0;
 
 echo "<br>DENTRO indSel: [$indSel]";
 
@@ -25,10 +138,7 @@ if (($indSel != '') and ($indSel != '0')) {
         $actProv = $row1['provincia'];
         $actCitta = $row1['citta'];
         $actPredInd = $row1['default'];
-
-
     }
-
 }
 
 if ($actUpd == 'AggiungiIndirizzo') {
@@ -42,6 +152,13 @@ if ($actUpd == 'AggiungiIndirizzo') {
     $nuovaCitta = $_POST['ind_citta'];
     $nuovoPredInd = $_POST['ind_checkbox_pred'];
 
+    //MM20231214 Gestisco gli ID di Regione, Provincia, Comune e Frazione
+
+    $nuovoIDRegione = $_POST['PreSelReg'] ?? '0';
+    $nuovoIDProvincia = $_POST['PreSelPrv'] ?? '0';
+    $nuovoIDComune = $_POST['PreSelCom'] ?? '0';
+    $nuovoIDFrazione = $_POST['PreSelFrz'] ?? '0';
+
     echo "<BR>Denominazione: $nuovaDenInd";
     echo "<BR>Tipo Indirizzo: $nuovoTipoInd";
     echo "<BR>Indirizzo $nuovoInd";
@@ -50,9 +167,14 @@ if ($actUpd == 'AggiungiIndirizzo') {
     echo "<BR>Città: $nuovaCitta";
     echo "<BR>Default: $nuovoPredInd";
 
+    echo "<BR>nuovoIDRegione: $nuovoIDRegione";
+    echo "<BR>nuovoIDProvincia: $nuovoIDProvincia";
+    echo "<BR>nuovoIDComune: $nuovoIDComune";
+    echo "<BR>nuovoIDFrazione: $nuovoIDFrazione";
+
 
     //se IDINdirizzo==0 faccio insert
-//se !=0 faccio update
+    //se !=0 faccio update
     if (($indSel == '') or ($indSel == '0')) {
         $sql_ins_ind = "INSERT INTO `indirizzi` 
             (`idutente`,
@@ -62,7 +184,8 @@ if ($actUpd == 'AggiungiIndirizzo') {
             `cap`,
             `provincia`,
             `citta`,
-            `default`
+            `default`,
+            `IDRegione`,`IDProvincia`,`IDComune`,`IDFrazione`
             ) VALUES
             ($idutente,
             '$nuovaDenInd',
@@ -71,7 +194,11 @@ if ($actUpd == 'AggiungiIndirizzo') {
             $nuovoCap,
             '$nuovaProv',
             '$nuovaCitta',
-            $nuovoPredInd); ";
+            $nuovoPredInd,
+            $nuovoIDRegione,
+            $nuovoIDProvincia,
+            $nuovoIDComune,
+            $nuovoIDFrazione); ";
 
         ExecuteSQL($sql_ins_ind);
     } else {
@@ -89,10 +216,12 @@ if ($actUpd == 'AggiungiIndirizzo') {
 <!-- Modal indirizzo-->
 <div id="ModalIndirizzo" class="modal" role="dialog">
     <div id="ModalIndirizzoContente" class="modal-dialog">
-        <!-- Modal content 
-<div id="ModalIndirizzo" class="modal fade" role="dialog">
-    <div id="ModalIndirizzoContente" class="modal-dialog">
--->
+
+        <input type='text' id='PreSelReg' name='PreSelReg' value='<?= $IDReg ?>'>
+        <input type='text' id='PreSelPrv' name='PreSelPrv' value='<?= $IDProv ?>'>
+        <input type='text' id='PreSelCom' name='PreSelCom' value='<?= $IDCom ?>'>
+        <input type='text' id='PreSelFrz' name='PreSelFrz' value='<?= $IDFrz ?>'>
+
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
@@ -132,8 +261,7 @@ if ($actUpd == 'AggiungiIndirizzo') {
                     <div class="grid-item-full">
                         <div class="form-group">
                             <label for="ind_denominazione">Nominativo:</label>
-                            <input type="text" class="form-control" id="ind_denominazione" name="ind_denominazione"
-                                placeholder="Inserisci il nominativo" required value='<?php echo $actDenInd; ?>'>
+                            <input type="text" class="form-control" id="ind_denominazione" name="ind_denominazione" placeholder="Inserisci il nominativo" required value='<?php echo $actDenInd; ?>'>
                         </div>
                     </div>
 
@@ -142,52 +270,92 @@ if ($actUpd == 'AggiungiIndirizzo') {
                     <div class="grid-item-full">
                         <div class="form-group">
                             <label for="ind_indirizzo">Indirizzo e nr civico:</label>
-                            <input type="text" class="form-control" id="ind_indirizzo" name="ind_indirizzo"
-                                placeholder="Inserisci la via" required value='<?php echo $actInd; ?>'>
+                            <input type="text" class="form-control" id="ind_indirizzo" name="ind_indirizzo" placeholder="Inserisci la via" required value='<?php echo $actInd; ?>'>
                         </div>
                     </div>
                 </div>
 
                 <div class="row">
+                    <!-- REGIONE -->
+                    <div class="col-md-3 col-sm-3 ml-4">
+                        <div class="form-group">
+                            <label for="cbo_reg" class="mb-3">Regione:</label>
+                            <select name="cbo_reg" id="cbo_reg">
+                                <option value='0'>Scegli...</option>
+                                <?php
+                                $sql = "SELECT * FROM vw_Regioni ORDER BY DESCRIZIONE";
+                                $res = GetData($sql);
+                                if ($res->num_rows > 0) {
+                                    while ($row = $res->fetch_assoc()) {
+                                        if ($IDReg == $row["NUMRIF"]) {
+                                            echo "<option value='" . $row["NUMRIF"] . "' selected>" . $row["DESCRIZIONE"] . "</option>";
+                                        } else {
+                                            echo "<option value='" . $row["NUMRIF"] . "'>" . $row["DESCRIZIONE"] . "</option>";
+                                        }
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- PROVINCIA -->
+                    <div class="col-md-3 col-sm-3 ml-4">
+                        <div class="form-group">
+                            <span id="span_prv" name="span_prv">
+                            <label for="cbo_com" class="mb-3">Provincia:</label>
+                                <select name='cbo_prv' id='cbo_prv'>
+                                    <option value='0'>Scegli...</option>
+                                </select>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- COMUNE -->
+                    <div class="col-md-3 col-sm-3 ml-4">
+                        <div class="form-group">
+                            <span id="span_com" name="span_com">
+                                <label for="cbo_com" class="mb-3">Comune:</label>
+                                <select name='cbo_com' id='cbo_com'>
+                                    <option value='0'>Scegli...</option>
+                                </select>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- FRAZIONE -->
+                    <div class="col-md-3 col-sm-3 ml-4">
+                        <div class="form-group">
+                            <span id="span_frz" name="span_frz">
+                                <label for="cbo_frz" class="mb-3">Frazione:</label>
+
+                                <select name='cbo_frz' id='cbo_frz'>
+                                    <option value='0'>Scegli...</option>
+                                </select>
+
+                            </span>
+                        </div>
+                    </div>
                     <div class="col-md-3 col-sm-3 ml-4">
                         <div class="form-group">
                             <label for="ind_cap">CAP:</label>
-                            <input type="text" class="form-control" name="ind_cap" id="ind_cap" placeholder="CAP"
-                                required maxlength="5" pattern="[0-9]{5}" value='<?php echo $actCap; ?>'>
+                            <input type="text" class="form-control" name="ind_cap" id="ind_cap" placeholder="CAP" required maxlength="5" pattern="[0-9]{5}" value='<?php echo $actCap; ?>'>
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-3 ml-4">
-                        <div class="form-group">
-                            <label for="ind_provincia" class="mb-3">Provincia:</label>
-                            <select id="ind_provincia" name="ind_provincia" class="mb-3" required>
-                                <option value="RM">Roma</option>
-                            </select>
-                        </div>
-
-                    </div>
-                    <div class="col-md-3 col-sm-3 ml-4">
-                        <div class="form-group">
-                            <label for="ind_citta" class="mb-3">Città:</label>
-                            <select id="ind_citta" name="ind_citta" class="mb-3" required>
-                                <option value="RM">Roma</option>
-
-
-                            </select>
-                        </div>
-                    </div>
-
                 </div>
+
                 <div class="row ml-4">
                     <input type="hidden" id="ind_checkbox_pred" name="ind_checkbox_pred" value="0">
-                    <label for="ind_checkbox_pred" class="checkbox-inline"><input type="checkbox" id="ind_checkbox_pred"
-                            name="ind_checkbox_pred" value="1">Imposta indirizzo come
+                    <label for="ind_checkbox_pred" class="checkbox-inline"><input type="checkbox" id="ind_checkbox_pred" name="ind_checkbox_pred" value="1">Imposta indirizzo come
                         predefinito</label>
                 </div>
             </div>
 
             <div class="modal-footer">
-                <button type="submit" class="btn btn-default left"
-                    onclick="AggiornaProfilo('AggiungiIndirizzo')">Aggiungi</button>
+                <button type="submit" class="btn btn-default left" onclick="AggiornaProfilo('AggiungiIndirizzo')">Aggiungi</button>
 
                 <button type="button" class="btn btn-default" data-dismiss="modal" onClick="ChiudiAdr()">Chiudi</button>
             </div>
