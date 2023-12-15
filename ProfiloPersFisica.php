ac9e59c7dbdb54dd069373f79bed19ee8ec3e829
MMascherin <massimiliano.mascherin@gmail.com>
2023-12-15 13:08:05 +0100

    Update ProfiloPersFisica.php
    Gestione dei comuni da combo
    ricercar belfiore
    Parametro da passare nella verifica del codice fiscale

--- ProfiloPersFisica.php
+++ ProfiloPersFisica.php
@@ -1,3 +1,50 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script>
+    $(document).ready(function() {
+
+        $('#pf_com').change(function() {
+            //Regione selezionata
+            IDCom = $('#pf_com').val();
+            console.log('Comune selezionata: ' + IDCom);
+            CercaBelfiore('COM', IDCom);
+
+        }); //Fine cbo_prv
+
+    }); //Fine document
+    
+    function CercaBelfiore(Tipo, ValSel) {
+        console.log('\r\CercaBelfiore');
+        var strEcho = '\r\nTipo: ' + Tipo + '\r\nValSel: ' + ValSel ;
+        console.log(strEcho);
+
+
+        $.ajax({
+            type: "POST",
+            url: "datiRepPrvComFrz.php",
+            data: {
+                'TIPO': Tipo,
+                'VAL_SEL': 0,
+                'PRV_SEL': 0,
+                'COM_SEL': ValSel,
+                'FRZ_SEL': 0
+            },
+            dataType: "JSON",
+
+            success: function(data) {
+
+                    if (data.BELFIORE != null) {
+                        console.log('\r\BELFIORE:' + data.BELFIORE);
+                        $('#belfiore').val(data.BELFIORE);
+                    }
+                
+            },
+
+            error: function() {
+                alert("Chiamata fallita, si prega di riprovare...");
+            }
+        }); //Fine Ajax Prov
+    }
+</script> 
 <div class="col-md-8" id="PersFisica">
 
     <?php
@@ -31,10 +78,6 @@
               echo "<br>codice fiscale: [" . $codice_fiscale . "]";
 
       */
-
-
-
-
     } else {
     }
     ?>
@@ -49,40 +92,61 @@
                 <!-- Form Group (first name)-->
                 <div class="col-md-6">
                     <label class="small mb-1" for="pf_inputName">NOME</label>
-                    <input class="form-control" id="pf_inputName" name="pf_inputName" type="text"
-                        placeholder="Inserisci il tuo nome" value="<?= $nome ?>">
+                    <input class="form-control" id="pf_inputName" name="pf_inputName" type="text" placeholder="Inserisci il tuo nome" value="<?= $nome ?>">
                 </div>
                 <!-- Form Group (last name)-->
                 <div class="col-md-6">
                     <label class="small mb-1" for="pf_inputLastName">COGNOME</label>
-                    <input class="form-control" id="pf_inputLastName" name="pf_inputLastName" type="text"
-                        placeholder="Inserisci il tuo cognome" value="<?= $cognome ?>">
+                    <input class="form-control" id="pf_inputLastName" name="pf_inputLastName" type="text" placeholder="Inserisci il tuo cognome" value="<?= $cognome ?>">
                 </div>
             </div>
             <div class="mb-3 gx-3">
                 <label class="small mb-1 " for="pf_inputCodFisc">CODICE FISCALE</label>
-                <input class="form-control" id="pf_inputCodFisc" type="text" name="pf_inputCodFisc" maxlength="16"
-                    placeholder="Inserisci il tuo Codice Fiscale" title="Inserisci un codice fiscale valido"
-                    value="<?= $codice_fiscale ?>">
+                <input class="form-control" id="pf_inputCodFisc" type="text" name="pf_inputCodFisc" maxlength="16" placeholder="Inserisci il tuo Codice Fiscale" title="Inserisci un codice fiscale valido" value="<?= $codice_fiscale ?>">
             </div>
 
             <!-- Form Row        -->
+            <div class="row">
+                <!-- REGIONE -->
+                <div class="col-md-3 col-sm-3 ml-4">
+                    <div class="form-group">
+                        <label for="pf_com" class="mb-3">Luogo di nascita:</label>
+                        <select name="pf_com" id="pf_com">
+                            <option value='0'>Scegli...</option>
+                            <?php
+                            $sql = "SELECT * FROM vw_Comuni ORDER BY DESCRIZIONE";
+                            $res = GetData($sql);
+                            if ($res->num_rows > 0) {
+                                while ($row = $res->fetch_assoc()) {
+                                    if ($IDReg == $row["NUMRIF"]) {
+                                        echo "<option value='" . $row["NUMRIF"] . "' selected>" . $row["DESCRIZIONE"] . "</option>";
+                                    } else {
+                                        echo "<option value='" . $row["NUMRIF"] . "'>" . $row["DESCRIZIONE"] . "</option>";
+                                    }
+                                }
+                            }
+                            ?>
+                        </select>
+                        <input type='text' name='belfiore' id='belfiore'> 
+                    </div>
+                </div>
+            </div>
+            <!--
             <div class="row gx-3 mb-3">
-                <!-- Form Group (organization name)-->
+                 
                 <div class="col-md-6">
                     <label class="small mb-1" for="pf_inputLuogoNascita">LUOGO DI NASCITA</label>
-                    <input class="form-control" id="pf_inputLuogoNascita" name="pf_inputLuogoNascita" type="text"
-                        placeholder="Inserisci il tuo luogo di nascita" value="<?= $luogo_di_nascita ?>">
+                    <input class="form-control" id="pf_inputLuogoNascita" name="pf_inputLuogoNascita" type="text" placeholder="Inserisci il tuo luogo di nascita" value="<?= $luogo_di_nascita ?>">
                 </div>
 
             </div>
+                        -->
             <div class="row gx-3 mb-3">
 
                 <!-- Form Group (birthday)-->
                 <div class="col-md-4">
                     <label class="small mb-1" for="pf_inputBirthday">DATA DI NASCITA</label>
-                    <input class="form-control" id="pf_inputBirthday" name="pf_inputBirthday" type="date"
-                        data-format="dd/MM/yyyy" placeholder="dd/MM/yyyy" value="<?= $data_di_nascita ?>">
+                    <input class="form-control" id="pf_inputBirthday" name="pf_inputBirthday" type="date" data-format="dd/MM/yyyy" placeholder="dd/MM/yyyy" value="<?= $data_di_nascita ?>">
                 </div>
 
                 <div class=" col-md-6 mt-4">
