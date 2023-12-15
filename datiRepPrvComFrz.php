fd8dc06d8ef095a9ae9f5ca1995753f0dc9e3bda
MMascherin <massimiliano.mascherin@gmail.com>
2023-12-15 11:56:45 +0100

    Update datiRepPrvComFrz.php
    Aggiunga gestione CAP e BELFIORE

--- datiRepPrvComFrz.php
+++ datiRepPrvComFrz.php
@@ -6,6 +6,7 @@ $prv_sel = $_POST["PRV_SEL"];
 $com_sel = $_POST["COM_SEL"];
 $frz_sel = $_POST["FRZ_SEL"];
 
+
 /*
 echo "<br>Tipo: " . $tipo;
 echo "<br>val_sel: " . $val_sel;
@@ -16,66 +17,80 @@ echo "<br>frz_sel: " . $frz_sel;
 
 require 'Config\SQL_Command.php';
 
-
-    $prvData='';
-    $pre_sel = $prv_sel;
-    $sql = "SELECT * FROM vw_Province WHERE  numrifpad=" . $val_sel . "  ORDER BY DESCRIZIONE";
-    $res = GetData($sql);
-    if ($res->num_rows > 0) {
-        while ($row = $res->fetch_assoc()) {
-            if ($pre_sel==$row["NUMRIF"]) {
-                $prvData .= "<option value='" . $row["NUMRIF"] ."' selected>" . $row["DESCRIZIONE"] ."</option>";
-            } else {
-                $prvData .= "<option value='" . $row["NUMRIF"] ."'>" . $row["DESCRIZIONE"] ."</option>";
-            }
+$prvData = '';
+$pre_sel = $prv_sel;
+$sql = "SELECT * FROM vw_Province WHERE  numrifpad=" . $val_sel . "  ORDER BY DESCRIZIONE";
+$res = GetData($sql);
+if ($res->num_rows > 0) {
+    while ($row = $res->fetch_assoc()) {
+        if ($pre_sel == $row["NUMRIF"]) {
+            $prvData .= "<option value='" . $row["NUMRIF"] . "' selected>" . $row["DESCRIZIONE"] . "</option>";
+        } else {
+            $prvData .= "<option value='" . $row["NUMRIF"] . "'>" . $row["DESCRIZIONE"] . "</option>";
         }
     }
-   
+}
 
-    $comData='';
-    $pre_sel = $com_sel;
-    $sql = "SELECT * FROM vw_Comuni WHERE  numrifpad=" . $prv_sel . "  ORDER BY DESCRIZIONE";
-    $res = GetData($sql);
-    if ($res->num_rows > 0) {
-        while ($row = $res->fetch_assoc()) {
-            if ($pre_sel==$row["NUMRIF"]) {
-                $comData .= "<option value='" . $row["NUMRIF"] ."' selected>" . $row["DESCRIZIONE"] ."</option>";
-            } else {
-                $comData .= "<option value='" . $row["NUMRIF"] ."'>" . $row["DESCRIZIONE"] ."</option>";
-            }
+
+$comData = '';
+
+$pre_sel = $com_sel;
+$sql = "SELECT * FROM vw_Comuni WHERE  numrifpad=" . $prv_sel . "  ORDER BY DESCRIZIONE";
+$res = GetData($sql);
+if ($res->num_rows > 0) {
+    while ($row = $res->fetch_assoc()) {
+        if ($pre_sel == $row["NUMRIF"]) {
+            $comData .= "<option value='" . $row["NUMRIF"] . "' selected>" . $row["DESCRIZIONE"] . "</option>";
+        } else {
+            $comData .= "<option value='" . $row["NUMRIF"] . "'>" . $row["DESCRIZIONE"] . "</option>";
         }
     }
-   
+}
 
-    $frzData='';
-    $pre_sel = $frz_sel;
-    $sql = "SELECT * FROM vw_Frazioni WHERE  numrifpad=" . $com_sel . "  ORDER BY DESCRIZIONE";
-    $res = GetData($sql);
-    if ($res->num_rows > 0) {
-        while ($row = $res->fetch_assoc()) {
-            if ($pre_sel==$row["NUMRIF"]) {
-                $frzData .= "<option value='" . $row["NUMRIF"] ."' selected>" . $row["DESCRIZIONE"] ."</option>";
-            } else {
-                $frzData .= "<option value='" . $row["NUMRIF"] ."'>" . $row["DESCRIZIONE"] ."</option>";
-            }
+
+$frzData = '';
+$pre_sel = $frz_sel;
+$sql = "SELECT * FROM vw_Frazioni WHERE  numrifpad=" . $com_sel . "  ORDER BY DESCRIZIONE";
+$res = GetData($sql);
+if ($res->num_rows > 0) {
+    while ($row = $res->fetch_assoc()) {
+        if ($pre_sel == $row["NUMRIF"]) {
+            $frzData .= "<option value='" . $row["NUMRIF"] . "' selected>" . $row["DESCRIZIONE"] . "</option>";
+        } else {
+            $frzData .= "<option value='" . $row["NUMRIF"] . "'>" . $row["DESCRIZIONE"] . "</option>";
         }
     }
- 
+}
 
-    
-    if ($tipo == 'ALL') {
-        $arData = array( 'PRV' => $prvData, 'COM'=>$comData, 'FRZ' => $frzData );
-    }
-    if ($tipo == 'REG') {
-        $arData = array( 'PRV' => $prvData );
-    }
-    if ($tipo == 'PRV') {
-        $arData = array( 'COM'=>$comData );
-    }
-    if ($tipo == 'COM') {
-        $arData = array( 'FRZ' => $frzData );
-    }
-    
- 
-    echo json_encode($arData);
 
+
+$cap= '';
+$belfiore = '';
+
+$sql = "SELECT * FROM vw_Comuni WHERE  numrif=" . $com_sel;
+$res = GetData($sql);
+if ($res->num_rows > 0) {
+    $row = $res->fetch_assoc();
+
+    $cap = $row["CAP"];
+    $belfiore = $row["BELFIORE"];
+}
+
+
+
+
+
+if ($tipo == 'ALL') {
+    $arData = array('PRV' => $prvData, 'COM' => $comData, 'FRZ' => $frzData);
+}
+if ($tipo == 'REG') {
+    $arData = array('PRV' => $prvData);
+}
+if ($tipo == 'PRV') {
+    $arData = array('COM' => $comData);
+}
+if ($tipo == 'COM') {
+    $arData = array('FRZ' => $frzData, 'CAP' => $cap, 'BELFIORE' => $belfiore);
+}
+
+echo json_encode($arData);
